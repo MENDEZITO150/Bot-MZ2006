7f119514c754fb9eb63fb262f65362c18e95aa57
MENDEZITO150 <96160874+MENDEZITO150@users.noreply.github.com>
2022-06-23 09:08:14 -0500

    Update menú.-menu.js

--- complementos/menú.-menu.js
+++ complementos/menú.-menu.js
@@ -439,8 +439,8 @@ conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/BrunoSobrino/TheMysti
 conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
 throw e
 }}
-handler.help = ['menu', 'help', '?']
-handler.tags = ['main']
+handler.help = ['/menu', 'help', '?']
+handler.tags = ['menu']
 handler.command = /^(menucompleto|menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos|m|\?)$/i
 handler.exp = 50
 handler.fail = null
