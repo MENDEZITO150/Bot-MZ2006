c7ffbd563716c2a730148f8e3e402b186e3b2856
MENDEZITO 150 <96160874+MENDEZITO150@users.noreply.github.com>
2022-07-17 12:57:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ const connectionOptions = {
 printQRInTerminal: true,
 auth: state,
 logger: P({ level: 'silent'}),
-browser: ['TheMystic-Bot','Edge','1.0.0']
+browser: ['BOT_MENDEZV2👹','Edge','1.0.0']
 }
 
 global.conn = makeWASocket(connectionOptions)
