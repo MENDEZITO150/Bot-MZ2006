a1be293c722fab59533a734c052b3be59f336f75
MENDEZITO 150 <96160874+MENDEZITO150@users.noreply.github.com>
2022-07-20 12:41:05 -0500

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -5,7 +5,7 @@ contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: nul
 title: '𝚂𝙲𝚁𝙸𝙿𝚃 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃 𝙼𝙳',
 body: 'ᴛʜᴇ ᴍʏsᴛɪᴄ ﹣ ʙᴏᴛ ﹣ ᴍᴅ',         
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
+sourceUrl: `https://github.com/MENDEZITO150/Bot-MZ2006`}}})
 }
 handler.command = ['sc','script']
 handler.help = ['sc']
