16d841391011bfba7b94bacb90f3bb3dec3f53cc
MENDEZITO 150 <96160874+MENDEZITO150@users.noreply.github.com>
2022-07-24 00:49:31 -0500

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,13 +1,12 @@
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝙴𝙽𝙻𝙰𝙲𝙴 𝙳𝙴 𝙸𝙽𝚂𝚃𝙰𝙶𝚁𝙰𝙼, 𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link*`
-const results = await instagramdl(args[0])
-.catch(async _ => await instagramdlv2(args[0]))
-.catch(async _ => await instagramdlv3(args[0]))
-.catch(async _ => await instagramdlv4(args[0]))
-for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${url}`, m)
+import fetch from 'node-fetch'
+let handler = async (m, { text, usedPrefix, command }) => {
+if (!text) throw `*[❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝚄𝙽 𝚃𝙴𝚇𝚃𝙾 𝙿𝙰𝚁𝙰 𝙷𝙰𝙱𝙻𝙰𝚁 𝙲𝙾𝙽 𝚂𝙸𝙼𝚂𝙸𝙼𝙸 𝙾 𝙴𝙻 𝙱𝙾𝚃*\n\n*𝙴𝙹𝙴𝙼𝙿𝙻𝙾: ${usedPrefix + command} Hola bot*`
+let res = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
+let json = await res.json()
+let tes = json.success.replace('simsimi', 'simsimi').replace('Simsimi', 'Simsimi').replace('sim simi', 'sim simi')
+m.reply(`${tes}`) 
 }
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command =/^(instagram|ig(dl)?)$/i
+handler.help = ['simsimi']
+handler.tags = ['General']
+handler.command = ['bot', 'simi', 'simsimi'] 
 export default handler
